d004328ea005561aa173c126886a89965c638a17
Oscar Hoekstra <oscar.hoekstra@wur.nl>
2019-01-28 12:25:59 +0100

    Fully updated the pipeline to v2

--- Pipeline_Thesis.py
+++ Pipeline_Thesis.py
@@ -8,15 +8,11 @@ Description: Runs the pipeline for linking the MIBiG and NPDB databases
 import time
 import sys
 import config
-from Scripts import GetSqlIDs, InchiToSQL
-#import InchiToSQL
+from Scripts import GetSqlIDs, InchiToSQL, ClassifyMibigCsv, MIBiGToSQL4
+from Scripts import Run_pyclassyfire4
+import pickle
 #import SQL_IDS_to_List
 #import Run_pyclassyfire4
-#import MIBiGToSQL4
-#import ClassifyMibigCsv
-#import sys
-#import time
-#import pickle
 #import os
 
 
@@ -59,15 +55,15 @@ if __name__ == "__main__":
         w.write("Arguments: "+str(sys.argv)+"\n")
 
 
-    if 1 not in SkipSteps:
+    # Can not skip step 1, NPDB_IDs is necessary for other steps
 #1 Get a list with all IDs present in the NPDB
-        print("__Starting Step 1__")
-        PrintTime()
-        NPDB_IDs = GetSqlIDs.main(cfg['SQLPath'],
-                                  cfg['NPDBtable'],
-                                  cfg['structure_id'])
-        Interval,start = interval(start)
-        print("Step 1 took "+Interval)
+    print("__Starting Step 1__")
+    PrintTime()
+    NPDB_IDs = GetSqlIDs.main(cfg['SQLPath'],
+                              cfg['NPDBtable'],
+                              cfg['structure_id'])
+    Interval,start = interval(start)
+    print("Step 1 took "+Interval)
 
     if 2 not in SkipSteps:
 #2 Get Inchi Keys from Sam/Rutger, Input into NPDB and combine 2
@@ -79,7 +75,7 @@ if __name__ == "__main__":
         Interval,start = interval(start)
         print("_____Step 2 took "+Interval)
 
-"""IM Here"""
+
     if 3 not in SkipSteps:
 #3 Put all of MIBiG into SQL Database and translate SMILES to Inchi_keys.
 #  Then add smiles obtained from Michelle Schorn and get the classifications
@@ -89,63 +85,54 @@ if __name__ == "__main__":
         #Adding Smiles from csv to mibig database
 
         #!!!
-        MibigCompoundDict = ClassifyMibigCsv.LoadMibigCsv(MIBiGsmilesFile)
+        MibigCompoundDict = ClassifyMibigCsv.LoadMibigCsv(cfg['MibigSmilesFile'])
         MIBiGToSQL4.main(cfg['SQLPath'],cfg['MibigTable'],MibigCompoundDict)
-        with open("PickledQueryIDDict.txt","rb") as f:
+        with open(cfg['PQueryID'],"rb") as f:
             QueryIDDict = pickle.load(f)
-        Run_pyclassyfire4.mainMIBIG(QueryIDDict,cfg['SQLPath'],MIBiGtableName,TimeStamp = cfg['StartTimestamp'])
-
-
-
-        #MibigCompoundDict = ClassifyMibigCsv.LoadMibigCsv(MIBiGsmilesFile)
-        #MIBiGToSQL4.main(cfg['SQLPath'],MIBiGtableName,MibigCompoundDict)
-        #with open("PickledQueryIDDict.txt","rb") as f:
-        #    QueryIDDict = pickle.load(f)
-        #Run_pyclassyfire4.mainInchiKey(QueryIDDict,cfg['SQLPath'],MIBiGtableName,TimeStamp = cfg['StartTimestamp'])
-
+        Run_pyclassyfire4.mainMIBIG(QueryIDDict,cfg['SQLPath'],cfg['MibigTable'],TimeStamp = cfg['StartTimestamp'])
 
         Interval,start = interval(start)
         print("_____Step 3 took "+Interval)
 
-"""
+    if 4 not in SkipSteps:
 #4 Run ClassyFire on NPDB and put results back into database
-    print("_____Starting Step 4")
-    PrintTime()
-    ToClassify = []
-    DoClassify = False #Set This to True to always re-do the classification
-    if os.path.isfile("ToClassify.txt") and DoClassify == False:
-        with open("ToClassify.txt", "rb") as f:
-            ToClassify = pickle.load(f)
-        if len(ToClassify) == 0:
-            print("Every Structure has already been classified.")
-            if input("Do you want to re-do the classification? (y/n)\n") == 'y':
-                DoClassify = True
-    if DoClassify == True: #if ToClassify.txt doesnt exist we will assume everything still has to be classified
-        with open("ToClassify.txt", "wb") as f:
-            ToClassify = NPDB_IDs
-            pickle.dump(NPDB_IDs, f)
+        print("_____Starting Step 4")
+        PrintTime()
+        ToClassify = Run_pyclassyfire4.GetToClassify(
+            cfg['RedoClassify'],
+            cfg['ToClassifyFile'],
+            NPDB_IDs)
 
-    BatchSize = 10
-    BatchedToClassify = [ToClassify[x:x+BatchSize] for x in range(0, len(ToClassify), BatchSize)]
-    Progress = 0
-    TotalNumber = len(ToClassify)
-    try:
-        Run_pyclassyfire4.AddColumns(cfg['SQLPath'],NPDBtableName)
-        for Batch in BatchedToClassify:
-            Run_pyclassyfire4.main(Batch, cfg['SQLPath'], NPDBtableName, Batched = True, TimeStamp = cfg['StartTimestamp'])
-            for item in Batch:
-                ToClassify.remove(item)
-            with open('ToClassify.txt', 'wb') as f:
-                pickle.dump(ToClassify, f) #basically removes the classified ids from the file
-            Progress += BatchSize
-            sys.stdout.write(str(Progress) +"/"+ str(TotalNumber) + " Structures Completed"+(" "*36))
-            sys.stdout.flush()
-            sys.stdout.write('\r')
-            sys.stdout.flush()
-    except KeyboardInterrupt:
-        print("You seem to have interupted the program while it was running PyClassyFire")
-        print("All batches that have finished have been saved to the SQL database")
-    print("_____Step 4 took "+Interval)
+        BatchSize = cfg['BatchSize']
+        if cfg['DoBatched'] == True:
+            BatchedToClassify = [ToClassify[x:x+BatchSize] for x in range(0, len(ToClassify), BatchSize)]
+        else:
+            # Works the same as batched, just with a single batch.
+            BatchedToClassify = [ToClassify]
+
+        Progress = 0
+        TotalNumber = len(ToClassify)
+        try:
+            Run_pyclassyfire4.AddColumns(cfg['SQLPath'],cfg['NPDBtable'])
+            for Batch in BatchedToClassify:
+                Run_pyclassyfire4.main(Batch, cfg['SQLPath'],
+                    cfg['NPDBtable'],InchiColumn="inchi_key",
+                    Batched = True, TimeStamp = cfg['StartTimestamp'])
+                for item in Batch:
+                    ToClassify.remove(item)
+                with open(cfg['ToClassifyFile'], 'wb') as f:
+                    pickle.dump(ToClassify, f) #basically removes the classified ids from the file
+                Progress += BatchSize
+                # Below writes and overwrites the progress, functioning
+                # sort of like a progress bar.
+                sys.stdout.write(str(Progress) +"/"+ str(TotalNumber) + " Structures Completed"+(" "*36))
+                sys.stdout.flush()
+                sys.stdout.write('\r')
+                sys.stdout.flush()
+        except KeyboardInterrupt:
+            print("You seem to have interupted the program while it was running PyClassyFire")
+            print("All batches that have finished have been saved to the SQL database")
+        print("_____Step 4 took "+Interval)
 
 
 #5 Combine MIBiG and NPDB somehow
@@ -153,4 +140,3 @@ if __name__ == "__main__":
     ScriptEnd = time.time()
     print("Pipeline Finished!")
     print("The whole script took: "+str(round(ScriptEnd - cfg['ScriptStartingTime']))+ " Seconds")
-    """
